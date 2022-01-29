8a24c20f37062d2d7748d99da7ca282755aed316
Nelson <punkpunkada@gmail.com>
2022-01-29 11:45:50 -0300

    consumindo api dentro de api e utilizando axios

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,6 +1,6 @@
 const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
-
+const axios = require('axios')
 class Atendimento {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
@@ -58,11 +58,14 @@ class Atendimento {
     buscaPorId(id, res) {
         const sql = `SELECT * FROM Atendimentos WHERE id=${id}`
 
-        conexao.query(sql, (erro, resultados) => {
+        conexao.query(sql, async (erro, resultados) => {
             const atendimento = resultados[0]
+            const cpf = atendimento.cliente
             if(erro) {
                 res.status(400).json(erro)
             } else {
+                const {data} = await axios.get(`http://localhost:8080/${cpf}`)
+                atendimento.cliente = data
                 res.status(200).json(atendimento)
             }
         })
