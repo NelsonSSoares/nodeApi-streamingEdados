da1d824d29bd9100262655edd7d54135da4ff953
Nelson <punkpunkada@gmail.com>
2022-02-01 15:46:16 -0300

    refatorando codigo e trabalhando com promisses

---
+++ infraestrutura/database/queries.js
@@ -0,0 +1,13 @@
+const conexao = require('./conexao');
+
+const executaQuery = (query, params = '') =>{
+    return new Promise((resolve, reject)=>{
+        conexao.query(query, params, (erros, resultados, campos)=>{
+            if(erros){
+                reject(erros)
+            }else{
+                resolve(resultados)
+            }
+        })
+    })
+}
