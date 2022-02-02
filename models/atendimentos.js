443e4894e90c7bf64a1127803d4f79edb5c7776f
Nelson <punkpunkada@gmail.com>
2022-02-02 10:53:14 -0300

    refatorando validação

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -3,27 +3,43 @@ const conexao = require('../infraestrutura/database/conexao')
 const axios = require('axios')
 const repositorio = ('../repositorios/atendimentos')
 class Atendimento {
-    adiciona(atendimento) {
-        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
 
-        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
-        const clienteEhValido = atendimento.cliente.length >= 5
+    constructor(){
 
-        const validacoes = [
+        this.dataEhValida = ({ data, dataCriacao})=> moment(data).isSameOrAfter(dataCriacao)
+        this.clienteEhValido = (tamanho) => tamanho >=5
+        this.valida = () => this.validacoes.filter(campo =>{
+            const {nome} = campo;
+            const parametro = parametros[nome]
+
+            return !campo.valido(parametro)
+        })
+        this.validacoes = [
             {
                 nome: 'data',
-                valido: dataEhValida,
+                valido: this.dataEhValida,
                 mensagem: 'Data deve ser maior ou igual a data atual'
             },
             {
                 nome: 'cliente',
-                valido: clienteEhValido,
+                valido: this.clienteEhValido,
                 mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
         ]
+    }
 
-        const erros = validacoes.filter(campo => !campo.valido)
+    adiciona(atendimento) {
+
+        
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+
+        const parametros = {
+            data:{data, dataCriacao} ,
+            cliente: {tamanho: atendimento.cliente.length}
+        }
+
+        const erros = this.valida(parametros)
         const existemErros = erros.length
 
         if (existemErros) {
